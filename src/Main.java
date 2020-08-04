c10459fbe264feb51eebac8e858ad923e5823558
Junseok <wnstjr2gkgk@gmail.com>
2020-08-04 17:50:57 +0900

    Action Tag HW

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main{  
+public static void main(String args[]){  
+Employee e=new Employee();//object is created  
+e.setName("Arjun");//setting value to the object  
+System.out.println(e.getName());  
+}}  
